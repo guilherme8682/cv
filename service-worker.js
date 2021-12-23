5f5a0e5ca5bdd12282ea1199cce9d6d48ab4536b
Guilherme Rocha <guilherme.8482@gmail.com>
2021-12-23 05:37:19 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cv/precache-manifest.e27f1b70faac99cc1ad787acce90c077.js"
+  "/cv/precache-manifest.8a7a3323354a3d36230667b965b2b40d.js"
 );
 
 workbox.clientsClaim();
